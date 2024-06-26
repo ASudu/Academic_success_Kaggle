ccfe42720a27ce1b31bb7204bef233f08964638e
A.Sudarshan <f20190744@pilani.bits-pilani.ac.in>
2024-06-26 19:07:43 +0530

    FCN

---
+++ FCN.py
@@ -0,0 +1,15 @@
+import torch
+import torch.nn as nn
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.fc1 = nn.Linear(50, 35)
+        self.fc2 = nn.Linear(35, 20)
+        self.fc3 = nn.Linear(20, 3)
+    
+    def forward(self, x):
+        x = torch.relu(self.fc1(x))
+        x = torch.relu(self.fc2(x))
+        x = self.fc3(x)  # CrossEntropyLoss includes softmax
+        return x
